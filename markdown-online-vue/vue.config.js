c4ce394c1c955eb636afd9e0925e44a2f6015b80
torch <1444504585@qq.com>
2022-06-18 19:59:26 +0800

    first commit

---
+++ markdown-online-vue/vue.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    open: true,
+    port: 80,
+    // hotOnly: false,
+    // disableHostCheck: true,
+    proxy: {
+        '/api': {
+            target: 'http://localhost:8080',
+            //公网地址
+            // http://sm2.v.frp.fit，
+            changeOrigin: true,// 允许跨域
+            ws: true, //如果要代理 websockets，配置这个参数
+            pathRewrite: {
+                '^/api': ''
+            }
+        }
+    }
+  }
+})
